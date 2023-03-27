85523b0fc3712ce780f0fa14b5c0cebef0f5b7b5
Jan Staelens <jan.staelens@skyline.be>
2023-03-27 10:54:43 +0200

    Add project files.

---
+++ StartKeyService_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StartKeyService_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("StartKeyService_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("C638C89D-C725-4681-BC2D-01287590A8B7")]
+[assembly: AssemblyVersion("1.0.0.0")]
